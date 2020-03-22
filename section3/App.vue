1409e898d08a354ed7a63179401d4d8ea537f600
Lastor <lastoraya@gmail.com>
2020-03-22 17:58:58 +0800

    [重構] section3，試用 Sass 功能
    - 巢狀嵌套
    - 變數
    - mixin

--- section3/App.vue
+++ section3/App.vue
@@ -4,7 +4,7 @@
       <div class="container position-relative">
         <div class="row align-items-end justify-content-between">
           <div class="man-img d-none d-lg-block">
-            <img src="https://colorlib.com/preview/theme/estore/assets/img/categori/card-man.png">
+            <img :src="manImg">
           </div>
           <div class="col-lg-4 d-none d-lg-block">
             <div class="effect-text">
@@ -25,93 +25,114 @@
         </div>
       </div>
       <div class="clothes-img jump-animate">
-        <img src="https://colorlib.com/preview/theme/estore/assets/img/categori/card-shape.png">
+        <img :src="clothesImg">
       </div>
     </div>
   </div>
 </template>
 
+<script>
+module.exports = {
+  data() {
+    return {
+      manImg: 'https://colorlib.com/preview/theme/estore/assets/img/categori/card-man.png',
+      clothesImg: 'https://colorlib.com/preview/theme/estore/assets/img/categori/card-shape.png'
+    }
+  }
+}
+</script>
+
 <style lang="scss">
+$bg: 'https://colorlib.com/preview/theme/estore/assets/img/categori/card.png';
+$font-title: playfair display, serif;
+$font-normal: poppins, sans-serif;
+
+@mixin bg-set($repeat, $position, $size) {
+  background: {
+    image: url($bg);
+    repeat: $repeat;
+    position: $position;
+    size: $size;
+  }
+}
+
+@mixin position-ctrl($pos, $top, $right, $bottom, $left) {
+  position: $pos;
+  top: $top;
+  right: $right;
+  bottom: $bottom;
+  left: $left;
+}
+
 .wrapper {
   padding: 10rem 2.5rem;
-  font-family: poppins, sans-serif;
-}
-
-.bg {
-  background: url(https://colorlib.com/preview/theme/estore/assets/img/categori/card.png) no-repeat;
-  background-position: right;
-  background-size: cover;
-}
-
-.man-img {
-  position: absolute;
-  left: 9%;
-  bottom: 0;
-  z-index: 1;
-}
-
-.clothes-img {
-  position: absolute;
-  right: 6%;
-  bottom: 15%;
-}
-
-.jump-animate {
-  animation: {
-    name: jump;
-    duration: 2s;
-    iteration-count: infinite;
+  font-family: $font-normal;
+  .bg {
+    @include bg-set(no-repeat, right, cover)
   }
-}
-
-.effect-text span {
-  font-size: 7.3rem;
-  text-transform: uppercase;
-  font-weight: bold;
-  writing-mode: vertical-lr;
-  transform: rotate(180deg);
-  -webkit-text-fill-color: transparent;
-  -webkit-text-stroke: rgba(231, 220, 198, 1) 1px;
-}
-
-.desc-area {
-  padding: 10rem 0;
-  h2 {
-    font-family: playfair display, serif;
-    font-size: 2.85rem;
-    font-weight: bold;
+  .man-img {
+    @include position-ctrl(absolute, null, null, 0, 9%);
+    z-index: 1;
   }
-  p {
-    font-size: 1.2rem;
+  .clothes-img {
+    @include position-ctrl(absolute, null, 6%, 15%, null);
   }
-  .shop-btn {
-    display: inline-block;
-    font-size: 1.2rem;
-    color: #fff;
-    background-color: #000;
-    padding: .85rem 3rem;
-    border: 1px solid #000;
-    border-radius: 2.5rem;
-    box-shadow: 0 10px 30px 0px rgb(191, 181, 153);
-    transition: transform .3s ease-out;
-    &:focus, &:hover {
-      text-decoration: none;
+  .jump-animate {
+    animation: {
+      name: jump;
+      duration: 2s;
+      iteration-count: infinite;
     }
-    &:hover {
-      transform: translateY(-10px)
+  }
+  .effect-text {
+    span {
+      font-size: 7.3rem;
+      font-weight: bold;
+      text-transform: uppercase;
+      writing-mode: vertical-lr;
+      transform: rotate(180deg);
+      -webkit-text-fill-color: transparent;
+      -webkit-text-stroke: rgba(231, 220, 198, 1) 1px;
+    }
+  }
+  .desc-area {
+    padding: 10rem 0;
+    h2 {
+      font: {
+        family: $font-title;
+        size: 2.85rem;
+        weight: bold;
+      }
+    }
+    p {
+      font-size: 1.2rem;
+    }
+    .shop-btn {
+      display: inline-block;
+      font-size: 1.2rem;
+      color: #fff;
+      background-color: #000;
+      padding: .85rem 3rem;
+      border: 1px solid #000;
+      border-radius: 2.5rem;
+      box-shadow: 0 10px 30px 0px rgb(191, 181, 153);
+      transition: transform .3s ease-out;
+      &:focus, &:hover {
+        text-decoration: none;
+      }
+      &:hover {
+        transform: translateY(-10px)
+      }
     }
   }
 }
 
 @keyframes jump {
-  0% {
+  0%, 100% {
     transform: translateY(0px);
   }
   50% {
     transform: translateY(-10px);
-  }
-  100% {
-    transform: translateY(0px);
   }
 }
 </style>
